6c2838d4c858cb195aaba673a61c6fd098783342
yeone <74540594+porcupine7021@users.noreply.github.com>
2025-07-21 01:31:39 +0900

    [level 0] Title: 글자 지우기, Time: 1.39 ms, Memory: 77.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181900. 글자 지우기/글자 지우기.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+
+class Solution {
+            public String solution(String my_string, int[] indices) {
+                Arrays.sort(indices);
+                StringBuilder sb = new StringBuilder(my_string);
+                for (int i = indices.length - 1; i >= 0; i--) {
+                    sb.deleteCharAt(indices[i]);
+                }
+                String answer = sb.toString();
+                return answer;
+            }
+        }
