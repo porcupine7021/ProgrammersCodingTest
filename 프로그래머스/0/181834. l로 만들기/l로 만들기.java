b79327cfad3bac6b4a9df2a448d8fe1413cbed99
yeone <74540594+porcupine7021@users.noreply.github.com>
2025-08-03 23:20:44 +0900

    [level 0] Title: l로 만들기, Time: 7.63 ms, Memory: 78.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181834. l로 만들기/l로 만들기.java
@@ -0,0 +1,13 @@
+ class Solution {
+    public String solution(String myString) {
+        
+        char[] chars = myString.toCharArray();
+        
+        for (int i = 0; i < chars.length; i++) {
+            
+            if (chars[i] < 'l') chars[i] = 'l';
+        }
+        
+        return new String(chars);
+    }
+}
