fe64f43d26a03824fba96b009fe4d0d8135bc739
yeone <74540594+porcupine7021@users.noreply.github.com>
2025-08-13 10:38:50 +0900

    [level 0] Title: 가위 바위 보, Time: 0.07 ms, Memory: 68.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.java
@@ -0,0 +1,14 @@
+class Solution {
+    public String solution(String rsp) {
+        
+        StringBuilder sb = new StringBuilder();
+        
+        for (char ch : rsp.toCharArray()) {
+            if (ch == '2') sb.append('0'); 
+            else if (ch == '0') sb.append('5'); 
+            else if (ch == '5') sb.append('2'); 
+        }
+        
+        return sb.toString();
+    }
+}
