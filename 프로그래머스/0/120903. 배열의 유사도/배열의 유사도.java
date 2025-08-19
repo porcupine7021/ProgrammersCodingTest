1d07b9a66108c087b553dd5c03da4b029eab06f1
yeone <74540594+porcupine7021@users.noreply.github.com>
2025-08-20 00:45:19 +0900

    [level 0] Title: 배열의 유사도, Time: 0.03 ms, Memory: 79.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/120903. 배열의 유사도/배열의 유사도.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+class Solution {
+    public int solution(String[] s1, String[] s2) {
+        
+        int answer = 0;
+            
+        for (String a : s1)
+            for (String b : s2)
+                if (a.equals(b)) answer++;
+        
+        return answer;
+    }
+}
