23790cefaf91233bf66dceb850a55ff79e643385
yeone <74540594+porcupine7021@users.noreply.github.com>
2025-09-15 20:21:06 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 14.65 ms, Memory: 98.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.java
@@ -0,0 +1,25 @@
+class Solution {
+    public String solution(String s) {
+        
+        String answer = "";
+        int idx = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+
+            if (c == ' ') {
+                answer += c;
+                idx = 0;
+            } else {
+                if (idx % 2 == 0) {
+                    answer += Character.toUpperCase(c);
+                } else {
+                    answer += Character.toLowerCase(c);
+                }
+                idx++;
+            }
+        }
+
+        return answer;
+    }
+}
