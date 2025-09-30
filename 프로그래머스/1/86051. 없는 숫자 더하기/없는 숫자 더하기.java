303d9a0598ab2ec4dea0c01866e7322194221eaa
yeone <74540594+porcupine7021@users.noreply.github.com>
2025-09-30 20:40:28 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.03 ms, Memory: 84.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int solution(int[] numbers) {
+        
+        int answer = 0;
+        
+        for (int i = 0; i <= 9; i++) {
+            answer += i;
+        }
+        
+        for (int n : numbers) {
+            answer -= n;
+        }
+        
+        return answer;
+    }
+}
